701d9a48e7b8ea11faf520ab84ca77ed117a615e
Thibaut <thibaut.sardan@gmail.com>
2018-10-08 15:48:19 +0200

    remove db.js interface and fix eth.js

--- src/index.js
+++ src/index.js
@@ -14,24 +14,22 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-const db = require('./interfaces/db');
 const eth = require('./interfaces/eth');
 const net = require('./interfaces/net');
 const parity = require('./interfaces/parity');
 const personal = require('./interfaces/personal');
-const privateMod = require('./interfaces/private');
+const private = require('./interfaces/private');
 const shh = require('./interfaces/shh');
 const signer = require('./interfaces/signer');
 const trace = require('./interfaces/trace');
 const web3 = require('./interfaces/web3');
 
 module.exports = {
-  db,
   eth,
   parity,
   net,
   personal,
-  private: privateMod,
+  private,
   shh,
   signer,
   trace,
